bd7014f6c7d4f7bea7b2b5dd3a8617b034e1dfab
Edward Vercoe <edward.vercoe@sydney.edu.au>
2019-11-19 12:24:25 +1100

    changes

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const Datastore = require('nedb');
 const cron = require('node-cron');
 const Pusher = require('pusher');
 const authorizeSpotify = require('./authorizeSpotify')
+const getAccessToken = require('./getAccessToken')
+const getRecentlyPlayed = require('./getRecentlyPlayed')
 
 const clientUrl = process.env.CLIENT_URL;
 
@@ -18,6 +20,34 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/login', authorizeSpotify)
+app.get('/callback', getAccessToken, (req, res, next) => {
+  db.insert(req.credentials, err => {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect(`${clientUrl}/?authorized=true`)
+    }
+  })
+})
+app.get('/history', (req, res) => {
+  db.find({}, (err, docs) => {
+    if (err) {
+      throw Error('Failed to retrieve documents')
+    }
+
+    const accessToken = docs[0].access_token
+    getRecentlyPlayed(accessToken)
+      .then(data => {
+        const arr = data.map(e => ({
+          played_at: e.played_at,
+          track_name: e.track.name,
+        }))
+
+        res.json(arr)
+      })
+      .catch(err => console.log(err))
+  })
+})
 
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
